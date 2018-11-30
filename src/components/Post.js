8c6ae3c2961b200c38a77708f1837435bcc5a8b4
André Chimicatti Fabrini <chimicatti@bitbucket.org>
2018-11-30 15:27:48 -0200

    finalizado

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,24 +3,44 @@ import React from 'react';
 import Comment from './Comment';
 
 export default class Post extends React.Component {
-    cpnstructor(props){
+    constructor(props){
         super(props);
 
         this.state = {
-            comments: [
-                { text: 'Bom post!' }
-            ]
-        }
+            comments: [],
+            newCommentText:''
+        };
 
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleTextChange = this.handleTextChange.bind(this);
     }
 
+    handleSubmit(e) {
+        this.setState({
+            comments:[
+                ...this.state.comments,
+                { text: this.state.newCommentText }
+            ]
+        });
+
+        this.setState({ newCommentText: ''});
+
+        e.preventDefault();
+    }
+
+    handleTextChange(e) {
+        this.setState({newCommentText: e.target.value})
+    }
 
     render () {
         return (
             <div>
                 <h2>{ this.props.title }</h2>
-                <form>
-                    <input />
+                <form onSubmit={this.handleSubmit}> 
+                    <input 
+                    value={this.state.newCommentText} 
+                    onChange={this.handleTextChange}
+                    />
                     <button type="submit">Comentar</button>
                 </form>
                 { this.state.comments.map((comment, index)=> {
